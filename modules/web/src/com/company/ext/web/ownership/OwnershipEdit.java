6fcd9bff9bfeb46c57692245ade594ec92c05360
sneoxx@mail.ru <zionnn7778>
2021-09-22 21:11:26 +0400

    2

---
+++ modules/web/src/com/company/ext/web/ownership/OwnershipEdit.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) ${YEAR} ${PACKAGE_NAME}
+ */
+
+package com.company.ext.web.ownership;
+
+import com.company.ext.entity.Ownership;
+import com.haulmont.cuba.gui.components.AbstractEditor;
+
+import java.util.Map;
+
+public class OwnershipEdit  extends AbstractEditor<Ownership> {
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+    }
+}
