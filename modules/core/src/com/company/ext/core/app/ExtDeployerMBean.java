97279e28492641170ceee6bd2ac811ef54b6d65e
sneoxx@mail.ru <zionnn7778>
2021-09-22 15:52:51 +0400

    Initial commit

---
+++ modules/core/src/com/company/ext/core/app/ExtDeployerMBean.java
@@ -0,0 +1,8 @@
+package com.company.ext.core.app;
+
+
+public interface ExtDeployerMBean{
+    String initDefault(String password);
+    
+
+}
---
+++ modules/global/src/com/company/ext/entity/Ownership.java
@@ -0,0 +1,42 @@
+package com.company.ext.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+import com.haulmont.cuba.core.entity.annotation.EnableRestore;
+import com.haulmont.cuba.core.entity.annotation.TrackEditScreenHistory;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Table(name = "EXT_OWNERSHIP")
+@Entity(name = "ext$Ownership")
+@EnableRestore
+@TrackEditScreenHistory
+@NamePattern("%s|name")
+public class Ownership extends StandardEntity {
+
+    @Column(name = "NAME", length = 100)
+    protected String name;
+
+    @Column(name = "CODE", nullable = false, length = 50)
+    protected String code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
