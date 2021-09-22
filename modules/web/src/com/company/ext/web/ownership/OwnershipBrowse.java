83b870ec9b267e9fadc45dfcb6f32e958c420e0f
sneoxx@mail.ru <zionnn7778>
2021-09-22 16:33:56 +0400

    1

---
+++ modules/web/src/com/company/ext/web/ownership/OwnershipBrowse.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) ${YEAR} ${PACKAGE_NAME}
+ */
+
+package com.company.ext.web.ownership;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+
+import java.util.Map;
+
+public class OwnershipBrowse extends AbstractLookup {
+
+    @Override
+    public void init(Map<String, Object> params) {
+    }
+}
